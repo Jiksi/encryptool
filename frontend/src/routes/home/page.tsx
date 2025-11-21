24a6cb543e22b9b200a3b673210d55f39c608788
Jiksi <zhicxiap99@gmail.com>
2025-11-21 14:09:35 +0800

    feat: integrate Tailwind CSS and shadcn

--- frontend/src/routes/home/page.tsx
+++ frontend/src/routes/home/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main>
-      <h1>Tes</h1>
+      <Button>Click me</Button>
     </main>
   );
 }
